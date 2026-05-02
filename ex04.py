7e6e764a222c781cf972f21dd538084d6925235f
lauurezende <lauurezendee@gmail.com>
2026-05-02 11:26:09 -0300

    Delete ex04.py

--- ex04.py
+++
@@ -1,7 +0,0 @@
-idade = int(input("Qual a sua idadae? "))
-experiencia = int(input("Quantos anos de experiência você tem? "))
-
-pode_acessar = idade >= 18 and experiencia >= 2
-
-print("Acesso liberado: ", pode_acessar)
-
